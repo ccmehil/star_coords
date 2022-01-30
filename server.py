c8f0a8d538167b1fa1241eb12af0bdfc6c9d7a03
Craig Cmehil <ccmehil@gmail.com>
2022-01-31 00:03:41 +0100

    Update server.py

--- server.py
+++ server.py
@@ -28,7 +28,6 @@ if not os.path.isfile(config_file):
     #Default to Greenwich as the site, 1 as tz
     h.write("[site]\n")
     h.write("\naddress=Greenwich\n")
-    h.write("\ntz=1\n")
     h.write("\nlatitude=51.6712\n")
     h.write("\nlongitude=8.3406\n")
     h.close()
@@ -38,7 +37,6 @@ config.read("server.ini")
 #server_name = config.get("server", "name") #e.g. 10.0.0.1
 #server_port = config.getint("server", "port") #e.g. 4030
 site_address = config.get("site", "address") #e.g. Greenwich
-site_tz = config.get("site", "tz") #e.g. 1
 site_latitude = config.get("site", "latitude") #e.g. 51.4176
 site_longitude = config.get("site", "longitude") #e.g. 8.1923
 
@@ -51,14 +49,6 @@ def debug_info(str):
     if debug:
         if debug_function in str:
             sys.stdout.write("%s\n" % str)
-
-def obs_time():
-    debug_info("FUNCTION obs_time")
-    now = dt.now()
-    times = [now]
-    t = Time(times, scale='utc')
-    obstime = Time(t) + np.linspace(0, 6, 10000) * u.hour
-    return obstime
 
 #Set local site (AltAz)
 location = EarthLocation.of_address(site_address)
