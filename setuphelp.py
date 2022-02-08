df8e60b530fe7f4531c6590433f35a2f6b686405
Craig Cmehil <ccmehil@gmail.com>
2022-02-08 23:45:14 +0100

    Update setuphelp.py

--- setuphelp.py
+++ setuphelp.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import requests
+import urllib.parse
 
 myaddress = 'Greenwich'
+url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(myaddress) +'?format=json'
 
-response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=%s' % myaddress)
-payload = response.json()
-print(payload['results'])
+response = requests.get(url).json()
+print("For %s the Latitude is %s and the Longitude is %s" % (myaddress, response[0]["lat"], response[0]["lon"]))
