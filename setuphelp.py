424ce03ee61bea864d72d2f32ff2228a5e6a27da
Craig Cmehil <ccmehil@gmail.com>
2022-02-08 23:46:07 +0100

    Update setuphelp.py

--- setuphelp.py
+++ setuphelp.py
@@ -7,3 +7,4 @@ url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(myaddre
 
 response = requests.get(url).json()
 print("For %s the Latitude is %s and the Longitude is %s" % (myaddress, response[0]["lat"], response[0]["lon"]))
+print ("run nano /home/pi/star_coords/server.ini and update the values there")
