b1481c0bd289677c1b098b240c088471d2a46a71
Craig Cmehil <ccmehil@gmail.com>
2022-02-08 23:42:51 +0100

    Update setuphelp.py

--- setuphelp.py
+++ setuphelp.py
@@ -5,4 +5,4 @@ myaddress = 'Greenwich'
 
 response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=%s' % myaddress)
 payload = response.json()
-print(payload['results'][0]['geometry']['location'])
+print(payload['results'])
